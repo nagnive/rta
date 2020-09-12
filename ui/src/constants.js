b294dd5ca65e4c3b3c9d45ddb27d5d06ac2da423
naguaddi <naguaddi@gmail.com>
2020-09-12 17:46:59 +0400

    Modified api url

--- ui/src/constants.js
+++ ui/src/constants.js
@@ -7,6 +7,4 @@ export const NORMAL = "normaluser";
 export const ADMIN = "adminuser";
 export const SUPER = "superuser";
 
-
-export const API_URL = "https://localhost:5001/";
-// export const API_URL = "http://3.236.51.65:8080/";
+export const API_URL = "http://3.236.51.65:8080/";
